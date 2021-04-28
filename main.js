b6ed8315377b73c216a6ff367a2aafb87419d8a0
jcccookie <doublekn88@gmail.com>
2021-04-27 21:38:09 -0700

    Add execution codes to a main program

--- main.js
+++ main.js
@@ -34,3 +34,16 @@ const numberToWord = numbers => {
 
   return results.join(", ");
 };
+
+//////////////////////////////////////////////////////
+// Program starts here /////////////////////////////// 
+//////////////////////////////////////////////////////
+
+// Get arguments from command line and store them to array as a string
+const numbers = getArgs();
+
+// Convert an array of integers into an array of strings representing the phonetic equivalent of the integer
+const words = numberToWord(numbers);
+
+// Print out the result on console
+console.log(words);
